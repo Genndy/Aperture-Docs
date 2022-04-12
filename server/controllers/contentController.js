38647947f6db47edf8aeb3817ca745d0e8c5017c
Genndy <Zabolotskiy-Gena@mail.ru>
2022-04-12 18:55:14 +0300

    Смена референса, добавление клиента

--- server/controllers/contentController.js
+++ server/controllers/contentController.js
@@ -1,39 +1,35 @@
 const uuid = require('uuid')
 const path = require('path')
-
+const {Content} = require('../models/models')
 const ApiError = require('../error/ApiError')
-const {Test} = require('../models/models')
 
-class UserController {
+class ContentController {
     async getAll(req, res) {
         let {limit, page} = req.query
         page = page || 1
         limit = limit || 9
         let offset = page * limit - limit
-        let tests;
-        tests = await Test.findAndCountAll({limit, offset})
-        return res.json(tests)
+        let contents;
+        contents = await Content.findAndCountAll({limit, offset})
+        return res.json(contents)
     }
 
     async create(req, res, next){
         try{
             let {docx, img, description} = req.body
-//            const {message} = req.body
-//            const {img} = req.files
-
             let docFileName = uuid.v4() + ".docx"
             let imgFileName = uuid.v4() + ".img"
             img.mv(path.resolve(__dirname, '..', 'static', imgFileName))
-            link.mv(path.resolve(__dirname, '..', 'static', imgFileName))
-            const test = await Test.create({docx, imgFileName, description})
-            return res.json(test)
+            docFileName.mv(path.resolve(__dirname, '..', 'static', imgFileName))
+            const content = await Content.create({docx, imgFileName, description})
+            return res.json(content)
         }catch(e){
             next(ApiError.badRequest(e.message))
         }
     }
 
     async getOne(req, res) {
-//        const {id} = req.id
+        //  const {id} = req.id
     }
 
     async removeOne(req, res){
@@ -41,4 +37,4 @@ class UserController {
     }
 }
 
-module.exports = new UserController()
+module.exports = new ContentController()
